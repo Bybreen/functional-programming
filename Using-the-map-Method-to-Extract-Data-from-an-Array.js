97d3bae12519a7570a1cf80d02b205b7a6fd2644
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-09-11 16:36:53 +0100

    Use the map Method to Extract Data from an Array
     Functions are considered First Class Objectsin JavaScript, which means they can be used like any other object. They can be saved in variables, stored in an object, or passed as function arguments.
    
    In this exercise we are looking at Array.prototype.map(), or more simply map.
    
    Remember that the mapmethod is a way to iterate over each item in an array. It creates a new array (without changing the original one) after applying a callback function to every element.

---
+++ Using-the-map-Method-to-Extract-Data-from-an-Array.js
@@ -0,0 +1,21 @@
+// the global variable
+
+// The watchListarray holds objects with information on several movies. 
+// Use mapto pull the title and rating from watchListand save the new array in the ratingvariable. 
+// The code in the editor currently uses a forloop to do this, replace the loop functionality with your mapexpression.
+
+var watchList = [ {
+'Title': 'Inception',
+'Year': '2010',
+'Rated': 'PG-13',
+'Released;: 'July 16th 2010',
+'Runtime': '148 minutes',
+'Genre': 'Action, Adventure, Crime',
+'Writer': 'Christopher Nolan',
+'http://ia.media-imdb.com/images
+'Metascore': '74',
+'imdbRatiing': '8.8',
+'Type': 'Movie',
+'Response': 'True',
+
+},
