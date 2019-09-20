3132df64d5ba0db1eae3fa09bdc3f38fe7c786a0
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-09-20 19:21:46 +0100

    Sorting an Array Alphabetically using the sort Method

---
+++ Sort-an-Array-Alphabetically-using-the-sort-Method.js
@@ -0,0 +1,7 @@
+// The sort method sorts the elements of an array according to the callback function.
+
+function alphabetOrder(arr);
+console.log(arr);
+return Arr;
+
+alphabeticalOrder([a, d, c, a, z, g]);
